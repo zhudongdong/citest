9328926bf3e0cafdd89dfd0bb7e9e9ec91053f7a
nierwin <mengjie_3751@126.com>
2016-01-19 13:31:45 +0800

    first commit

---
+++ citest-all/citest-app/src/main/java/com/zdd/citest/CITest.java
@@ -0,0 +1,7 @@
+package com.zdd.citest;
+
+public class CITest {
+	public void test() {
+		System.out.println("hello travis");
+	}
+}
---
+++ citest-all/citest-app/src/test/java/com/zdd/citest/test/CITestTest.java
@@ -0,0 +1,14 @@
+package com.zdd.citest.test;
+
+import org.junit.Test;
+
+import com.zdd.citest.CITest;
+
+public class CITestTest {
+
+	@Test
+	public void test() {
+		CITest test = new CITest();
+		test.test();
+	}
+}
